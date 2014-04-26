1c3036fd25a0ba21889c86fea3eb55d4a78e59bf
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-04-25 22:38:01 -0400

    wip

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -8,13 +8,8 @@ if(!function_exists('elasticsearch\includeIfExists')){
 	}
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
-    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL;
-    exit(1);
-}
+// if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {}
 
-$loader->add(null, __DIR__.'/../src', true);
+//$loader->add(null, __DIR__.'/../src', true);
 
-return $loader;
+//return $loader;
