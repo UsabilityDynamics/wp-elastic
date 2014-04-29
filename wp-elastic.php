3ea66709f1c59979c9f59c395757107db04924d1
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-04-28 20:37:23 -0400

    wip

--- wp-elastic.php
+++ wp-elastic.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: WP-Elastic
- * Plugin URI: http://wordpress.org/extend/plugins/fantastic-elasticsearch/
+ * Plugin URI: http://wordpress.org/extend/plugins/wp-elastic/
  * Description: Improve wordpress search performance and accuracy by leveraging an ElasticSearch server.
  * Version: 2.4.0
  * Author: Usability Dynamics, Inc.
- * Author URI: http://www.linkedin.com/in/parisholley
- * Author Email: mail@parisholley.com
+ * Author URI: http://www.usabilitydynamics.com/
+ * Author Email: info@usabilitydynamics.com
  * Network: true
  *
  **/
@@ -14,16 +14,22 @@
 // Include bootstrap.
 include_once( __DIR__ . '/lib/class-bootstrap.php' );
 
-// Initialize.
-function wp_elastic( $key = null ) {
+/**
+ * wp_elastic handler
+ *
+ * @param null $key
+ * @return null|\wpElastic\Bootstrap
+ */
+function wp_elastic( $key = null, $default = null ) {
   global $wp_elastic;
 
   if( !$wp_elastic ) {
-    $wp_elastic = new wpElastic\Bootstrap();
+    new wpElastic\Bootstrap();
   }
 
-  return $key ? $wp_elastic->get( $key ) : $wp_elastic;
+  return $key ? $wp_elastic->get( $key, $default ) : $wp_elastic;
 
 }
 
+// Initialize.
 wp_elastic();
